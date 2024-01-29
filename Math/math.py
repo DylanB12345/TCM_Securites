63d847782cec2725e3afeee95e3872a5b48afd51
Dylan <02Dylan.bellinger@gulllakecs.org>
2024-01-29 14:31:17 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,14 @@
+#Programmer: Dylan
+#Programmer: Math
+#Programmer: 1.10.2024
+
+print(9 * 9) #Multiply
+print(9 + 9) #addition
+print(9 - 6) #Subtraction
+print(9 / 3) #division
+print(50 + 50 - 50 * 50 / 50) #pemdas
+print(50 ** 2) # Exponents
+print(50  % 6) #Modulo - takes what is leftover
+print(50 / 6) #division with remainder for a float
+print(50 // 6) #whole number with no remainder
+
