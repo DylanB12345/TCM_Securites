2e272407404ca3f03a0d27433beeb51825a86305
Dylan <02Dylan.bellinger@gulllakecs.org>
2024-01-29 14:30:06 -0500

    strings

---
+++ Strings/first.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Programer: Dylan
+#Program: Strings
+
+print("Michigan Wolverines are National Champions\n")
+print("7 + 8 =", 7+8)
+print("")
+print("9 + 3 = "+ str( 9+3), "\n")
+
+print("""\n\n\n\nsHappy New Tear!
+everyone, Yeah!!!""") #""" will print on multiple lines"""
